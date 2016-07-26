cd6d5249216bc1d24a5cf0758e06b28a02ba53af
Jakir Hossain <jakir.hossain@gagagugu.com>
2016-07-26 22:12:22 +0600

    add firebase admob demo

---
+++ FirebaseAdmob/app/src/main/java/me/jakir/firebaseadmob/MainActivity.java
@@ -0,0 +1,26 @@
+package me.jakir.firebaseadmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+    }
+}
