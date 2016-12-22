0b23007c06aa30772ac0e23773fbd70ce0f9a857
jakir hossain <jakir hossain>
2016-12-22 22:52:04 +0600

    add splash screen tutorial

---
+++ AndroidSplashScreen/app/src/main/java/com/techtweets/androidsplashscreen/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.techtweets.androidsplashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+
+                // Start your app main activity after time is over
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
